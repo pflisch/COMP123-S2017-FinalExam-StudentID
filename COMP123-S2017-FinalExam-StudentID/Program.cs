3921ec65b7bfe8794eb8744972ad0c44215ee698
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-08-17 08:52:35 -0400

    Add project files.

---
+++ COMP123-S2017-FinalExam-StudentID/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    public static class Program
+    {
+        public static PickHighestCardForm pickHighestCardForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            pickHighestCardForm = new PickHighestCardForm();
+
+            Application.Run(new SplashForm());
+        }
+    }
+}
