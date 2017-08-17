8ec4a36fabf7207896e6850523ee0e7859e47ec1
pflisch <priffrodrigues@gmail.com>
2017-08-17 12:03:13 -0400

    added more information

---
+++ COMP123-S2017-FinalExam-StudentID/ScoreBoard.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    class ScoreBoard
+    {
+
+        // Private Instances
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
+
+        // Public Properties
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                ScoreTextBox.Text = Convert.ToString(this._score);
+                FinalScoreTextBox.Text = Convert.ToString(this._score);
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+            }
+        }
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                TimeTextBox.Text = Convert.ToString(this._time);
+            }
+        }
+        public TextBox TimeTextBox
+        {
+            get
+            {
+                return this._timeTextBox;
+            }
+            set
+            {
+                this._timeTextBox = value;
+            }
+        }
+
+        // Constructors
+        public ScoreBoard(TextBox scoreTextBox, TextBox TimeTextBox, TextBox finalScoreTextBox)
+        {
+
+        }
+        // Private methods
+
+        // Public methods
+        public void UpdateTime()
+        {
+
+        }
+    }
+}
